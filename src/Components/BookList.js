409308fd3f1221d8f78c3c3b48c4d56f05dea3c9
Cory Parravano <parravano.cory@gmail.com>
2017-10-05 21:32:40 -0700

    fix: Added key to BookList

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -44,29 +44,31 @@ class BookItem extends Component {
  render() {
 const book = this.props.books;
     return (
-
-  <div className="book">
-    <div className="book-top">
-      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageThumbnail})` }}></div>
-      <div className="book-shelf-changer">
-        <select>
-          <option value="none" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    </div>
-    <div className="book-title">{book.title}</div>
-    <div className="book-authors">{book.authers}</div>
-  </div>
-  )}
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageThumbnail})` }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authers}</div>
+        </div>
+      </li>
+    )
+  }
 }
 class BookList extends Component {
 
   render() {
-    const book= this.props.books
+
 
     return (
       <div className="list-books">
@@ -76,9 +78,7 @@ class BookList extends Component {
           <div className="list-books-content">
             <ol className="books-grid">
               {this.props.books.map((book) => (
-                <li key={book.id}>
-                <BookItem books={book}/>
-              </li>
+                <BookItem key={book.id} books={book}/>
               ))}
             </ol>
         </div>
