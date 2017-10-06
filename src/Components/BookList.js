367b72482500dd85c628c49f21ccdcee0375f414
Cory Parravano <parravano.cory@gmail.com>
2017-10-06 03:54:21 -0700

    feat: added BookShelf component to BookList
    add Bookshelf to return and title props for BookShelf title

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,71 +1,5 @@
 import React, { Component } from 'react'
-
-// class BookShelves extends Component {
-//   render() {
-//     return (
-//       <div className="list-books">
-//             <div className="list-books-title">
-//               <h1>MyReads</h1>
-//             </div>
-//             <div className="list-books-content">
-//               <div>
-//                 <div className="bookshelf">
-//                   <h2 className="bookshelf-title">Currently Reading</h2>
-//                   <div className="bookshelf-books">
-//                     <ol className="books-grid">
-//
-//                     </ol>
-//                   </div>
-//                 </div>
-//                 <div className="bookshelf">
-//                   <h2 className="bookshelf-title">Want to Read</h2>
-//                   <div className="bookshelf-books">
-//                     <ol className="books-grid">
-//
-//                     </ol>
-//                   </div>
-//                 </div>
-//                 <div className="bookshelf">
-//                   <h2 className="bookshelf-title">Read</h2>
-//                   <div className="bookshelf-books">
-//                     <ol className="books-grid">
-//
-//                     </ol>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//     )}
-// }
-
-
-class BookGrid extends Component {
-  render() {
-    const book = this.props.books
-
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageThumbnail})` }}></div>
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authers}</div>
-          </div>
-        </li>
-    )
-  }
-}
+import BookShelf from './BookShelf'
 
 class BookList extends Component {
   render() {
@@ -75,15 +9,11 @@ class BookList extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <div className="list-books-content">
-            <ol className="books-grid">
-              {this.props.books.map((book) => (
-                <BookGrid key={book.id} books={book}/>
-              ))}
-            </ol>
+          <BookShelf title='Currently Reading' books={this.props.books}/>
+          <BookShelf title='Want to Read' books={this.props.books}/>
+          <BookShelf title='Read' books={this.props.books}/>
         </div>
       </div>
-    </div>
     )
   }
 }
