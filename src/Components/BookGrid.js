f728c3e8a343d25054ca245972559d81094b5e7d
Cory Parravano <parravano.cory@gmail.com>
2017-10-14 20:43:24 -0700

    feat: Creat handle for Bookshelf

--- src/Components/BookGrid.js
+++ src/Components/BookGrid.js
@@ -2,15 +2,17 @@ import React, {Component} from 'react'
 
 class BookGrid extends Component {
 
+
   render() {
     const {book} = this.props
+
     return (
       <li>
         <div className="book">
           <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks})` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select value={book.shelf} onChange={(e) => this.state.onUpdateShelf(book, event.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -2,31 +2,71 @@ import React, { Component } from 'react'
 import BookGrid from './BookGrid'
 
 
-class BookList extends Component {
+// class BookList extends Component {
+//
+//   render() {
+//     return (
+//       <div className="list-books">
+//         <div className="list-books-title">
+//           <h1>MyReads</h1>
+//         </div>
+//           <div className="list-books-content">
+//                {this.props.bookShelves.map((bookShelf) => (
+//             <div key={bookShelf} className="bookshelf">
+//               <h2 className="bookshelf-title">{bookShelf}</h2>
+//                 <div className="bookshelf-books">
+//                   <ol className="books-grid">
+//                     {this.props.books.map((book) => (
+//                       <BookGrid
+//                         key={book.id}
+//                         book={book}/>
+//                     ))}
+//                   </ol>
+//               </div>
+//             </div>
+//           ))}
+//           </div>
+//       </div>
+//     )
+//   }
+// }
 
-  render() {
+class BookList extends Component {
+  render () {
+    const {books} = this.props
+
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-          <div className="list-books-content">
-            {this.props.bookShelves.map((bookShelf) => (
-            <div key={bookShelf}className="bookshelf">
-              <h2 className="bookshelf-title">{bookShelf}</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {this.props.books.map((book) => (
-                      <BookGrid
-                        key={book.id}
-                        book={book}/>
-                    ))}
-                  </ol>
-              </div>
-            </div>
-          ))}
-          </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf onUpdateShelf={this.onUpdateShelf} books={books} shelfTitle='Currently Reading' />
+            <BookShelf onUpdateShelf={this.onUpdateShelf} books={books} shelfTitle='Want to Read' />
+            <BookShelf onUpdateShelf={this.onUpdateShelf} books={books} shelfTitle='Read' />
+        </div>
       </div>
+    </div>
+    )
+  }
+}
+
+class BookShelf extends Component {
+  render () {
+    return (
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {this.props.books.map((book) => (
+                  <BookGrid
+                    key={book.id}
+                    book={book}/>
+                ))}
+              </ol>
+          </div>
+        </div>
     )
   }
 }
