10427e3ecccdfe86882bf6eaaef7838a0308c342
Cory Parravano <parravano.cory@gmail.com>
2017-10-20 03:06:13 -0700

    fix: prevouis merge

--- src/Components/BookSearch.js
+++ src/Components/BookSearch.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react'
 import BookGrid from './BookGrid'
 import * as BooksAPI from '../BooksAPI'
-<<<<<<< HEAD
-import escapeRegExp from 'escape-string-regexp'
-
-||||||| merged common ancestors
-=======
 
 
->>>>>>> a45a81c249cc5659160dbfc4f79b5f187d7eea51
 class BookSearch extends Component {
 
     state = {
@@ -16,62 +10,30 @@ class BookSearch extends Component {
       results: []
     }
 
-<<<<<<< HEAD
-||||||| merged common ancestors
-    updateQuery = query => {
-      this.setState({ query: query })
-    }
-=======
+
+
     onUpdateQuery = query => {
-    if(query === '' ) {
-      this.setSet({ query: '', results: [] })
-    } else {
-      BooksAPI.search(query, 20).then( results  => {
->>>>>>> a45a81c249cc5659160dbfc4f79b5f187d7eea51
+         if(query === '') {
+           this.setSet({ query: '', books: [] })
+         } else {
+           BooksAPI.search(query, 20).then( results  => {
+             if (results.length > 0) {
+               results = results.map(result => {
+               for (let book of this.props.books)
+               if (result.shelf === book.shelf ) {
+                 book.shelf = result.shelf
+               } else {
+                 result.shelf = 'none'
+               }
+               return result
+               })
+             }
+             this.setState({ results: results})
+           })
+           this.setState({query: event.target.value})
+         }
+       }
 
-<<<<<<< HEAD
-
-   onUpdateQuery = query => {
-    if(query === '' ) {
-      this.setSet({ query: '', results: [] })
-    } else {
-      BooksAPI.search(query, 20).then( results  => {
-        if (results.error){
-          console.log(results.error)
-          results = []
-        } else {
-||||||| merged common ancestors
-   getResults = query => {
-      if(query === '') {
-        this.setSet({ query: '', books: [] })
-      } else {
-        BooksAPI.search(query, 20).then( results  => {
-=======
->>>>>>> a45a81c249cc5659160dbfc4f79b5f187d7eea51
-          if (results.length > 0) {
-            results = results.map(result => {
-            for (let book of this.props.books)
-            if (result.shelf === book.shelf ) {
-              book.shelf = result.shelf
-              } else {
-                result.shelf = 'none'
-              }
-                return result
-              })
-            }
-<<<<<<< HEAD
-          }
-||||||| merged common ancestors
-            return result
-            })
-          }
-=======
->>>>>>> a45a81c249cc5659160dbfc4f79b5f187d7eea51
-          this.setState({ results: results})
-        })
-        this.setState({query})
-      }
-    }
 
   render() {
     const { results, query } = this.state
